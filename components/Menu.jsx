1fca39f806abc564a29b3d41910bd7f8f8e4b781
par jin sung <parkgutime@gmail.com>
2023-04-14 15:49:22 +0900

    [add] sidebar 작업 중

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,8 +1,12 @@
 // dom제어  useRef
 import React, { useEffect } from "react";
-import Link from "next/link";
 import { AiOutlineLeft } from "react-icons/ai";
 import { useStateContext } from "../context/StateContext";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { links } from "../data/dummy";
+import { Link } from "react-router-dom";
+import ProfileHeader from "../public/image/profileHeader.svg";
 
 const Menu = () => {
   const { setShowMenu } = useStateContext();
@@ -10,13 +14,34 @@ const Menu = () => {
   return (
     <div className="profile-wrapper">
       <div className="profile-container">
-        <button
-          type="button"
-          className="cart-heading"
-          onClick={() => setShowMenu(false)}
-        >
-          <AiOutlineLeft />
-        </button>
+        <div className="profile-header">
+          <button
+            type="button"
+            className="cart-heading"
+            onClick={() => setShowMenu(false)}
+          >
+            <AiOutlineLeft />
+          </button>
+          <ProfileHeader />
+        </div>
+        <div className="profile-menu">
+          {links.map((item) => (
+            <div key={item.title}>
+              <p>{item.title}</p>
+              {item.links.map((link) => (
+                // link 처리가 안돼서 일단
+                <p
+                  to={`/${link.name}`}
+                  key={link.name}
+                  // onClick={handleCloseSideBar}
+                >
+                  {link.icon}
+                  <span className="capitalize">{link.name}</span>
+                </p>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

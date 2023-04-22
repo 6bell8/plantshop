46dfcaf9d66285b458b08289233c45ef01bd5fef
par jin sung <parkgutime@gmail.com>
2023-04-22 12:23:07 +0900

    [modify] next/dynamic import

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import "../css/globals.css";
 import { Layout } from "../components";
 import React from "react"; // _app에서 import해서 react 돔을 실행해야 index가 동작
 import { StateContext } from "../context/StateContext";
+import dynamic from "next/dynamic";
+
+const ComponentsWithNoSSR = dynamic(
+  () => import("../pages/ecommerce"), // Component로 사용할 항목을 import합니다.
+  { ssr: false }
+); // ssr옵션을 false로 설정해줍니다.
 
 function App({ Component, pageProps }) {
   return (
@@ -10,6 +16,7 @@ function App({ Component, pageProps }) {
     <StateContext>
       <Layout>
         <Component {...pageProps} />
+        <ComponentsWithNoSSR />
       </Layout>
     </StateContext>
   );

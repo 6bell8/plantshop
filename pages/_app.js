2898736b84dbb1d9bcd4982781b60adc81bd2d55
par jin sung <parkgutime@gmail.com>
2023-04-24 14:28:14 +0900

    [test] vercel

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,17 @@ import React from "react"; // _app에서 import해서 react 돔을 실행해야 
 import { StateContext } from "../context/StateContext";
 import dynamic from "next/dynamic";
 
-const ComponentsWithNoSSR = dynamic(
-  () => import("../pages/ecommerce"), // Component로 사용할 항목을 import합니다.
-  { ssr: false }
-); // ssr옵션을 false로 설정해줍니다.
+const ComponentsWithNoSSR = dynamic(() => import("../pages/ecommerce"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 function App({ Component, pageProps }) {
   return (
-    // StateContext에서 전달받은 state를 app.js에서 묶어준다
-
     <StateContext>
       <Layout>
         <Component {...pageProps} />
-        <ComponentsWithNoSSR />
+        {/* <ComponentsWithNoSSR /> */}
       </Layout>
     </StateContext>
   );

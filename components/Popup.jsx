13c88f8f64f2d9ac8242e12ee58c74fa06114601
par jin sung <parkgutime@gmail.com>
2023-05-10 23:30:30 +0900

    [add] popup 제작 중

--- components/Popup.jsx
+++ components/Popup.jsx
@@ -4,23 +4,30 @@ import cookie from "react-cookies";
 const Popup = () => {
   // 이름을 바꿨더니 쿠키리업로드
 
-  useEffect(() => {
-    const expires = new Date();
-    expires.setDate(expires.getDate() + 5);
-    cookie.save("Cookiee", "abc", {
-      path: "/",
-      expires,
-    });
+  const [visited, setVisited] = useState(true);
+  const [popupActive, setPopupActive] = useState(true);
+  const [popupChecked, setPopupChecked] = useState(false);
 
-    setTimeout(function () {
-      alert(cookie.load("Cookiee"));
-    }, 1000);
-  });
+  useEffect(() => {
+    checkCookie("Cookiee");
+  }, []);
 
   return (
-    <div>
+    <div className={`popup-box ${popupActive ? "active" : ""}`}>
       <button className="delCookie" onClick={delCookie}>
         쿠키삭제
+      </button>
+      <label>
+        <input
+          type="checkbox"
+          className="popup"
+          checked={popupChecked}
+          onChange={(e) => setPopupChecked(e.target.checked)}
+        />
+        하루 안보기
+      </label>
+      <button className="close" onClick={popupClose}>
+        닫기
       </button>
     </div>
   );
@@ -28,7 +35,51 @@ const Popup = () => {
   //  함수가 실행이 되어도 바로 지워지는 것이 아님. 지워도 이따가 지워지고 새로고침하면 다시 쿠키생성 반복
   function delCookie() {
     cookie.remove("Cookiee", { path: "/" });
-    alert(cookie.load("Cookiee"));
+    // alert(cookie.load("Cookiee"));
+  }
+
+  function creatCookie() {
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 1);
+    cookie.save("Cookiee", "abc", {
+      path: "/",
+      expires,
+    });
+
+    setTimeout(function () {
+      //   alert(cookie.load("Cookiee"));
+    }, 1000);
+  }
+
+  //   쿠키 일치함수
+  function checkCookie(name) {
+    const cookies = document.cookie.split(";");
+    console.log(cookies);
+    for (let i in cookies) {
+      if (cookies[i].indexOf(name) > -1) {
+        setVisited(false);
+        if (visited === true) {
+          setPopupActive(true);
+        } else {
+          //   처음이 아니면 신규
+          setPopupActive(false);
+        }
+      }
+      console.log(visited);
+    }
+  }
+  //   쿠키가 생성이 됐는데 visited부분이 계속 false로 안뜸 반복문이 잘못된 듯
+
+  function popupClose() {
+    if (popupChecked === true) {
+      //팝업을 다시 안보겠다, 팝업을 닫고 쿠키생성
+      creatCookie();
+      setPopupActive(false);
+    } else {
+      //팝업을 계속 보겠다. 팝업을 닫고 쿠키를 제거
+      setPopupActive(false);
+      delCookie();
+    }
   }
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../css/globals.css";
 import { Layout, Loading, Popup } from "../components";
 import React, { useState } from "react"; // _app에서 import해서 react 돔을 실행해야 index가 동작
 import { StateContext } from "../context/StateContext";
+import { CookiesProvider } from "react-cookie";
 import Router from "next/router";
 
 function App({ Component, pageProps }) {
@@ -16,12 +17,14 @@ function App({ Component, pageProps }) {
 
   return (
     <StateContext>
-      <Layout>
-        {/* useState 논리연산자:  먼저 나온 게 false면 안보여줌 */}
-        <Popup />
-        {loading && <Loading />}
-        <Component {...pageProps} />
-      </Layout>
+      <CookiesProvider>
+        <Layout>
+          {/* useState 논리연산자:  먼저 나온 게 false면 안보여줌 */}
+          <Popup />
+          {loading && <Loading />}
+          <Component {...pageProps} />
+        </Layout>
+      </CookiesProvider>
     </StateContext>
   );
 }

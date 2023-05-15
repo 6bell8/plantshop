a7cf5864e7feb87686eff9b398bd8a8cbe37c17c
par jin sung <parkgutime@gmail.com>
2023-05-15 22:18:06 +0900

    [add] localstorage 오류 수정

--- components/Localviewed.jsx
+++ components/Localviewed.jsx
@@ -5,21 +5,7 @@ import { urlFor } from "../lib/client";
 
 const Localviewed = () => {
   const router = useRouter();
-  //   let [viewedProduct, setViewedProduct] = useState([]);
-  //   로컬 스토리지
 
-  useEffect(() => {
-    // 일단 로컬스토리지 파싱
-    //   let viewedProduct = JSON.parse(localStorage.getItem("watched"));
-
-    if (localStorage.getItem("watched") === null) {
-      localStorage.setItem("watched", JSON.stringify([]));
-    }
-  }, []);
-
-  let viewedProduct = JSON.parse(localStorage.getItem("watched"));
-
-  console.log(viewedProduct);
   return (
     <div className="viewed-wrapper">
       <div className="viewed-box">
